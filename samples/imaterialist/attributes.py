b776ae2af5cffc680a6295ed0638a76f4e246ee0
soon-yau <soonyau@sooner.ai>
2019-06-05 19:36:56 +0000

    attribs training scrips

--- samples/imaterialist/attributes.py
+++ samples/imaterialist/attributes.py
@@ -24,7 +24,7 @@ from tqdm import tqdm
 import imutils
 
 
-from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import RepeatedKFold, RepeatedStratifiedKFold
 
 import torch
 import torch.nn as nn
@@ -37,13 +37,13 @@ import torchsummary
 from torchvision import models, transforms
 import torchvision
 
-
+import imgaug.augmenters as iaa
 # In[2]:
 
 
 IMAGE_DIR = Path('/home/ubuntu/efs/kaggle/imaterialist/train')
 DATA_DIR = Path('/home/ubuntu/efs/kaggle/imaterialist/')
-ROOT_DIR = Path('/home/ubuntu/efs/kaggle/imaterialist/maskrcnn/logs')
+ROOT_DIR = Path('/home/ubuntu/efs/kaggle/imaterialist/pytorch/logs')
 
 # For demonstration purpose, the classification ignores attributes (only categories),
 # and the image size is set to 512, which is the same as the size of submission masks
@@ -67,7 +67,7 @@ attributes_list = [i['name'] for i in label_descriptions['attributes']]
 
 
 class AttribsDataset(Dataset):
-    def __init__(self, df):
+    def __init__(self, df, transforms):
         self.n_attributes = 92
         '''
         # create dataset
@@ -82,16 +82,17 @@ class AttribsDataset(Dataset):
         self.df = pd.concat([segment_with_attribs, segment_without_attribs])
         '''
         self.df = df
-        
+        self.transforms = transforms
+
     def __len__(self):
         return len(self.df)
     
     def __getitem__(self,idx):
         row = self.df.iloc[idx]
-        image = Image.open(os.path.join(IMAGE_DIR, row.ImageId))
+        image = cv2.imread(os.path.join(IMAGE_DIR, row.ImageId))
 
         # mask out
-        h, w = row.Height, row.Width
+        h, w,_ = image.shape#row.Height, row.Width
         mask = np.zeros((h*w), dtype=np.uint8)
         encodedPixels = list(map(int,row.EncodedPixels.split(' ')))
         for startPos, runLen in zip(encodedPixels[0::2], encodedPixels[1::2]):
@@ -116,10 +117,14 @@ class AttribsDataset(Dataset):
         #    print(attrib, attributes_list[int(attrib)])
 
         labels = np.zeros((self.n_attributes), np.float32)
-        labels[np.array(list(map(int,attributes)))] = 1
+        if len(attributes)>0:
+            labels[np.array(list(map(int,attributes)))] = 1
         
-        resized_cropped = np.float32(resized_cropped.transpose((2, 0, 1)))/255. -1
-        
+        if self.transforms:
+            resized_cropped = self.transforms.augment_image(resized_cropped)
+
+        resized_cropped = np.float32(resized_cropped.transpose((2, 0, 1)))/127.5 - 1
+
         return resized_cropped, np.float32(class_id), labels
     
     def resize_apsect_ratio(self, img, dim=512):
@@ -148,31 +153,69 @@ idx_with_attribs = segment_df['ClassId'].str.contains('_')
 idx_without_attribs = ~idx_with_attribs
 
 segment_with_attribs = segment_df[idx_with_attribs]
+df = segment_with_attribs[segment_with_attribs['ClassId'].str.split('_').str[0]<="12"]
+
+'''
 n_with_attribs = len(segment_with_attribs)
+
 # make total to be multiples of 32
-n_without_attribs = int(n_with_attribs//32*32*1.5-n_with_attribs)
-segment_without_attribs = pd.DataFrame.sample(segment_df[idx_without_attribs], n=n_without_attribs) 
+n_without_attribs = int(n_with_attribs*1.5//32*32-n_with_attribs)
+
+# exlucde cat < 13 that are not annotated
+segment_without_attribs = segment_df[idx_without_attribs]
+segment_without_attribs = segment_without_attribs[segment_without_attribs['ClassId'].str.split('_').str[0]>"12"]
+segment_without_attribs = pd.DataFrame.sample(segment_without_attribs, n=n_without_attribs) 
 
 df = pd.concat([segment_with_attribs, segment_without_attribs])
-assert(len(df)%32==0)
 
+print("with attribs", n_with_attribs)
+print("without attribs", n_without_attribs)
+'''
+print("total", len(df))
 
+#assert(len(df)%32==0)
+
+class_list = df['ClassId'].str.split('_').str[0].tolist()
+print("class_list type, len", type(class_list), len(df))
 # In[6]:
 
 
-rkf = RepeatedKFold(n_splits=6, n_repeats=10, random_state=8888)
-df_folder = rkf.split(df)
+skf = RepeatedStratifiedKFold(n_splits=6, n_repeats=10, random_state=8888)
+df_folder = skf.split(df, class_list)
+#rkf = RepeatedKFold(n_splits=8, n_repeats=10, random_state=8888)
+#df_folder = rkf.split(df)
+'''
+train_transforms = transforms.Compose([
+                         transforms.RandomHorizontalFlip(),
+                         transforms.RandomAffine(degrees=40, translate=(0.1,0.1), scale=(0.9,1.1)),
+                         transforms.ToTensor(),
+                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+val_transforms = transforms.Compose([
+                         transforms.ToTensor(),
+                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+'''
+augmentation = iaa.Sequential([
+    iaa.Fliplr(0.5), # only horizontal flip here
+    # rotate and translation
+    iaa.Affine(
+        scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},
+        translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)},
+        rotate=(-10, 10)),
+])
+
 
 def gen_dataset():
     train_index, val_index = next(df_folder)
     train_df = df.iloc[train_index]
-    valid_df = df.iloc[val_index]
+    val_df = df.iloc[val_index]
     
-    train_dataset = AttribsDataset(train_df)
+    train_dataset = AttribsDataset(train_df, augmentation)
+    val_dataset = AttribsDataset(val_df, None)
+    print("train and val size", len(train_dataset), len(val_dataset))
 
-    valid_dataset = AttribsDataset(valid_df)
-    
-    return train_dataset, valid_dataset
+    return train_dataset, val_dataset
 
 
 # # Model
@@ -187,20 +230,23 @@ class Resnet(nn.Module):
         
         self.base = nn.Sequential(*list(base_model.children())[:-1])
         self.fc = nn.Sequential(
-                        nn.Linear(2060,500),
+                        nn.Linear(2048,1024),
                         nn.ReLU(),
-                        nn.Linear(500,92),
+                        nn.Linear(1024,512),
+                        nn.ReLU(),
+                        nn.Linear(512,92),
                         nn.Sigmoid())
                 
     def forward(self, images, class_id):
         features = self.base(images)
+        y = self.fc(features.view(-1,2048))
         #class_id_repeat = class_id.repeat(12,1,1,1)
-        class_id_repeat = class_id.view(-1,1, 1,1).repeat(1,12,1,1)
+        #class_id_repeat = class_id.view(-1,1, 1,1).repeat(1,12,1,1)
         #print(features.shape, class_id.shape, class_id_repeat.shape)
-        x = torch.cat((features, class_id_repeat), dim=1)
-        x = x.view((-1,2060))
+        #x = torch.cat((features, class_id_repeat), dim=1)
+        #x = x.view((-1,2060))
 
-        y = self.fc(x)
+        #y = self.fc(x)
         return y
     
 model = Resnet().to("cuda")
@@ -215,13 +261,13 @@ model = nn.DataParallel(model)
 
 
 def Train(model, criterion, optimizer, scheduler, num_epochs=25):
-    log = SummaryWriter()
     logdir = "/home/ubuntu/efs/kaggle/imaterialist/checkpoints/attribs/%s"%                    datetime.now().strftime('%Y%m%d-%H%M')
     
     if os.path.isdir(logdir) != True:        
         os.makedirs(logdir)
         print("Create directory ", logdir)
 
+    log = SummaryWriter(logdir)
     #best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = 10000.0
     step = 0
@@ -231,8 +277,8 @@ def Train(model, criterion, optimizer, scheduler, num_epochs=25):
         if epoch%10 == 0:
             print("Getting new fold of dataset")
             train_dataset, val_dataset = gen_dataset()
-            dataLoaders = {'train':DataLoader(train_dataset, batch_size=batch_size, num_workers=8),
-                           'val':DataLoader(val_dataset, batch_size=batch_size, num_workers=8)}
+            dataLoaders = {'train':DataLoader(train_dataset, batch_size=batch_size, num_workers=32, shuffle=True),
+                           'val':DataLoader(val_dataset, batch_size=batch_size, num_workers=32)}
 
         # Each epoch has a training and validation phase
         for phase in ['train','val']:
@@ -267,15 +313,6 @@ def Train(model, criterion, optimizer, scheduler, num_epochs=25):
                 # statistics
                 image_count += images.size(0)
                 running_loss += loss.item() * images.size(0)
-                if phase =='train' and step%10 == 0:
-                    timeElapsed = time.time() - since
-                    since = time.time()
-                    meanLoss = running_loss/image_count
-                    print('[%d] training loss=%.4f Time in %.0f m %.0f s'%                          (step, loss.item(), timeElapsed//60, timeElapsed%60))                    
-                        
-                    # log
-                    log.add_scalar("train_loss", loss.item(), step)
-                    log.add_scalar("learning_rate", scheduler.get_lr()[0], step)
 
                 step+=1
                 
@@ -294,6 +331,15 @@ def Train(model, criterion, optimizer, scheduler, num_epochs=25):
                     modelName = '%s/model_step_%d.pth' % (logdir, step)
                     torch.save(model.state_dict(), modelName)
                     print("Save model ",modelName)
+            else:
+                    timeElapsed = time.time() - since
+                    since = time.time()
+                    meanLoss = running_loss/image_count
+                    print('[%d] training loss=%.4f Time in %.0f m %.0f s'%                          (step, loss.item(), timeElapsed//60, timeElapsed%60))                    
+                        
+                    # log
+                    log.add_scalar("train_loss", loss.item(), step)
+                    log.add_scalar("learning_rate", scheduler.get_lr()[0], step)
         #print('{} Loss: {:.4f}'.format(
             #    phase, epoch_loss))
 
@@ -309,12 +355,16 @@ def Train(model, criterion, optimizer, scheduler, num_epochs=25):
 
 
 # Optimizer
-learningRate = 1e-3
+learningRate = 1e-4
 
 nEpoch = 100
 lrDecayStep = 3
 lrDecayRate = 0.95
-
+'''
+for name, param in model.named_parameters():
+    print(name)
+    param.requires_grad = False if "module.fc." not in name
+'''
 criterion = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr = learningRate)
 expDecayOptimizer = optim.lr_scheduler.StepLR(optimizer, step_size=lrDecayStep, gamma=lrDecayRate)
@@ -323,7 +373,7 @@ expDecayOptimizer = optim.lr_scheduler.StepLR(optimizer, step_size=lrDecayStep, 
 # In[9]:
 
 
-batch_size = 4
+batch_size = 32
 bestModel = Train(model, criterion, optimizer, expDecayOptimizer, nEpoch)
 
 
